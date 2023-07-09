26d065ee87443f1daf95b8e1e599b4326089a981
Tom Klotz <tom.klotz@teamviewer.com>
2023-07-09 23:56:38 +0200

    set up

---
+++ libraries.R
@@ -0,0 +1,10 @@
+### Setup ----------------------------------------------------------------------
+
+#install.packages("pacman")
+library(pacman)
+p_load(quanteda,quanteda.textplots, ggplot2, tidyverse, devtools) #für BA?
+devtools::install_github("jogrue/popdictR")
+devtools::install_github("jogrue/regexhelpeR")
+devtools::install_github("jogrue/multidictR")
+
+### ----------------------------------------------------------------------------
